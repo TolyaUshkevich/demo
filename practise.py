a5e0dee25458d6c41390af11e186c47415b01bd3
TolIk <anatoliyyushkevich@gmail.com>
2022-06-08 22:32:06 +0300

    commit

--- practise.py
+++ practise.py
@@ -1,1 +1,16 @@
-print('Hello world:)')
+def find_annagrams(first, second):
+    first = set(first)
+    second = set(second)
+    if ' ' in first:
+        first.remove(' ')
+    if ' ' in second:
+        second.remove(' ')
+    count = 0
+    for i in first:
+        if i in second:
+            count += 1
+    if count == len(first) and count == len(second):
+        return 'ok'
+    else:
+        return 'non'
+print(find_annagrams(input(), input()))
